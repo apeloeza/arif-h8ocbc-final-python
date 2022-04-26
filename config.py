6c91b91c2d6c6f927023180d9bd3eafc2d4fe352
ApeloezA <m.arif.r123@gmail.com>
2022-04-27 04:33:25 +0700

    Add heroku deployment files

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ app = connex_app.app
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = True
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'final_proj.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hhepibilweibhh:77f09f1fd89db483be10fbe11a6d3771717267fc4db2967928607bf5e8bc41e3@ec2-3-223-213-207.compute-1.amazonaws.com:5432/dfp9a0oe61hqbv'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://iutcpfmvgtajxg:74c38d46aaf7bf5f46e72987e0c523f1d58bb85f3328cead4bfc96146885c1fe@ec2-3-211-6-217.compute-1.amazonaws.com:5432/df7nbe968vmjql'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
--- test.py
+++ test.py
@@ -22,63 +22,7 @@ class TestAPI(unittest.TestCase):
         response = data.get("/api/best_movies")
         self.assertEqual(response.status_code, 200)
 
-    def test_flop_movie(self):
-        connex_app.app.testing=True
-        data = connex_app.app.test_client(self)
-        response = data.get("/api/flop_movies")
-        self.assertEqual(response.status_code, 200)
-
-    def test_least_budget(self):
-        connex_app.app.testing=True
-        data = connex_app.app.test_client(self)
-        response = data.get("/api/budget/least")
-        self.assertEqual(response.status_code, 200)
-        
-    def test_most_budget(self):
-        connex_app.app.testing=True
-        data = connex_app.app.test_client(self)
-        response = data.get("/api/budget/most")
-        self.assertEqual(response.status_code, 200)
-        
-
-    def test_least_vote(self):
-        connex_app.app.testing=True
-        data = connex_app.app.test_client(self)
-        response = data.get("/api/vote/least")
-        self.assertEqual(response.status_code, 200)
-        
-    def test_most_vote(self):
-        connex_app.app.testing=True
-        data = connex_app.app.test_client(self)
-        response = data.get("/api/vote/most")
-        self.assertEqual(response.status_code, 200)
-        
-
-    def test_least_revenue(self):
-        connex_app.app.testing=True
-        data = connex_app.app.test_client(self)
-        response = data.get("/api/revenue/least")
-        self.assertEqual(response.status_code, 200)
-        
-    def test_most_revenue(self):
-        connex_app.app.testing=True
-        data = connex_app.app.test_client(self)
-        response = data.get("/api/revenue/most")
-        self.assertEqual(response.status_code, 200)
-        
-
-    def test_least_popular(self):
-        connex_app.app.testing=True
-        data = connex_app.app.test_client(self)
-        response = data.get("/api/popular/not")
-        self.assertEqual(response.status_code, 200)
-        
-    def test_most_popular(self):
-        connex_app.app.testing=True
-        data = connex_app.app.test_client(self)
-        response = data.get("/api/popular/most")
-        self.assertEqual(response.status_code, 200)
-        
+          
 
 
 if __name__ == '__main__':
